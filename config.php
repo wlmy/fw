1a22cbd471e44799fd46064565751ef92c0b2f16
wulimin <wulimin@hfjy.com>
2017-10-19 16:40:51 +0800

    add config

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'DB_TYPE' => 'mysql',         //数据库类型
+    'DB_HOST' => 'localhost',     //主机
+    'DB_NAME' => 'fuwu',          //数据库名称
+    'DB_USER' => 'root',          //用户名
+    'DB_PWD' => 'root',           //密码
+    'DB_PORT' => '3306'           //端口号
+);
+
+
--- index.php
+++ index.php
@@ -7,25 +7,29 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die ('Your PHP Version is ' . PHP_VERSION . ', But fw require PHP > 5.3.0 !');
 }
 
-
 class core
 {
+    /**
+     * 插入、更新数据
+     * @param $dbConnection
+     * @return string
+     */
     public function handleData($dbConnection)
     {
         //参数过滤
-        $request_data = $this->filter_request_data($_POST);
+        $request_data = $this->filter_request_params($_POST);
 
         $sel_res = $dbConnection->prepare('SELECT * FROM contract WHERE `contractId` = :contractId');
         $sel_res->execute(array(':contractId' => $request_data['contractId']));
         $sel_result = $sel_res->fetchAll(PDO::FETCH_ASSOC);
 
         if (!empty($sel_result)) {
-            $params = $sel_result[0];
-            $sel_res = $dbConnection->prepare('UPDATE contract SET `minutePerPeriod` = :minutePerPeriod, `unitPrice` = :unitPrice WHERE `contractId` = :contractId');
+            $sel_res = $dbConnection->prepare('UPDATE contract SET `minutePerPeriod` = :minutePerPeriod, `unitPrice` = :unitPrice, `update_time` = :update_time WHERE `contractId` = :contractId');
             $up_result = $sel_res->execute(array(
                 ':minutePerPeriod' => $request_data['minutePerPeriod'],
                 ':unitPrice' => $request_data['unitPrice'],
-                ':contractId' => $params['contractId']
+                ':contractId' => $request_data['contractId'],
+                ':update_time' => date('Y-m-d H:i:s')
             ));
             if ($up_result) {
                 return $this->print_to('0', '更新成功！');
@@ -47,35 +51,70 @@ class core
         } else {
             return $this->print_to('1', '插入失败！');
         }
-
     }
 
+    /**
+     * 查询数据
+     * @param $dbConnection
+     * @return string
+     */
     public function selectData($dbConnection)
     {
         //参数过滤
-        $request_data = $this->filter_request_data($_POST);
+        $request_data = $this->filter_request_params($_POST);
 
         $sel_res = $dbConnection->prepare('SELECT * FROM contract WHERE `contractId` = :contractId');
         $sel_res->execute(array(':contractId' => $request_data['contractId']));
         $sel_result = $sel_res->fetchAll(PDO::FETCH_ASSOC);
-
         foreach ($sel_result as $key => $row) {
             return $this->print_to('0', '查询成功！', [
-                'contractId' => $row['contractId'],
-                'minutePerPeriod' => $row['minutePerPeriod'],
-                'unitPrice' => $row['unitPrice']
+                'contractId' => stripcslashes($row['contractId']),
+                'minutePerPeriod' => stripcslashes($row['minutePerPeriod']),
+                'unitPrice' => stripcslashes($row['unitPrice'])
             ]);
         }
     }
 
     /**
-     * 过滤request参数并进行防sql注入处理
+     * 检测request参数防sql注入
+     * @param $data
+     * @return array
      */
-    public function filter_request_data($params)
+    public function filter_request_params($data)
     {
-        $request_data = $params;
-        return $request_data;
+        if (is_array($data)) {
+            foreach ($data as $key => $value) {
+                if (!is_array($value)) {
+                    if ($value !== '' && !is_null($value)) {
+                        //检测空格、tab、换行
+                        $value = trim($value);
 
+                        //get_magic_quotes_gpc()未开启情况下，数据过滤
+                        if (!get_magic_quotes_gpc()) {
+                            $value = addslashes($value);
+                        }
+
+                        //检测数字
+                        if (is_numeric($value)) {
+                            $value = intval($value);
+                        }
+
+                        //检测字符串
+                        if (is_string($value)) {
+                            $keyword = 'select|insert|update|delete|\/\*|\*|\.\.\/|\.\/|union|into|load_file|outfile';
+                            $arr = explode('|', $keyword);
+                            $value = str_ireplace($arr, '', $value);
+                            $value = nl2br($value); // 回车转换
+                        }
+                    }
+
+                    $data[$key] = $value;
+                } else {
+                    $this->filter_request_params($value[$key]);
+                }
+            }
+        }
+        return $data;
     }
 
     /**
@@ -95,12 +134,16 @@ class core
     }
 }
 
+
 $act = $_GET['act'] ? $_GET['act'] : '';
 if (empty($act)) {
     exit;
 } else {
+    $C = require("config.php");
+
     //建立PDO数据库连接
-    $dbConnection = new PDO('mysql:dbname=fuwu;host=localhost;charset=utf8', 'root', 'root');
+    $dbConnection = new PDO("mysql:host={$C['DB_HOST']};port={$C['DB_PORT']};dbname={$C['DB_NAME']};charset=utf8",
+        $C['DB_USER'], $C['DB_PWD']);
     $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
